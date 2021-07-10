74b16ad0ec421a031f496a4112cc3bb544bd09bd
lmorais <leozonnn@gmail.com>
2021-07-09 21:58:17 -0300

    feat: implementation abstract and in memory repository

--- src/modules/tools/repositories/IToolsRepository.ts
+++ src/modules/tools/repositories/IToolsRepository.ts
@@ -3,6 +3,7 @@ import Tool from '../infra/typeorm/entities/Tool';
 
 interface IToolsRepository {
   create(data: ICreateToolsDTO): Promise<Tool>;
+  findByTitle(title: string): Promise<Tool | undefined>;
 }
 
 export default IToolsRepository;
--- src/modules/tools/repositories/fakes/FakeToolRepository.ts
+++ src/modules/tools/repositories/fakes/FakeToolRepository.ts
@@ -1,9 +1,15 @@
+import { v4 as uuidv4 } from 'uuid';
+
 import ICreateToolDTO from '../../dtos/ICreateToolsDTO';
 import Tool from '../../infra/typeorm/entities/Tool';
 import IToolRepository from '../IToolsRepository';
 
 class FakeCreateToolRepository implements IToolRepository {
   private tools: Tool[] = [];
+
+  public async findByTitle(title: string): Promise<Tool | undefined> {
+    return this.tools.find((tool) => tool.title === title);
+  }
 
   public async create({
     title,
@@ -14,6 +20,7 @@ class FakeCreateToolRepository implements IToolRepository {
     const tool = new Tool();
 
     Object.assign(tool, {
+      id: uuidv4(),
       title,
       description,
       link,
