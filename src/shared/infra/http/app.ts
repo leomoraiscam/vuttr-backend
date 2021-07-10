2e49239ec979072f5b349d3cc5c223fffdf04193
lmorais <leozonnn@gmail.com>
2021-07-10 15:40:49 -0300

    feat: use middleware to when exist a exception error

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,6 +1,26 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+
 import '../../container/index';
+import AppError from '../../errors/AppError';
 
 const app = express();
+app.use(express.json());
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    console.log(`Error: ${error}`);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error',
+    });
+  }
+);
 
 export default app;
