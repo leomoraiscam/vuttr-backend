3028166c9f01816a73ed2cc402e89111794e719f
lmorais <leozonnn@gmail.com>
2021-07-11 15:49:40 -0300

    chore: add midleware to use routes in app

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -2,9 +2,11 @@ import express, { Request, Response, NextFunction } from 'express';
 
 import '../../container/index';
 import AppError from '../../errors/AppError';
+import routes from './routes';
 
 const app = express();
 app.use(express.json());
+app.use(routes);
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
