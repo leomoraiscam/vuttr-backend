5bd02b4b6df12b1bb45834ba27ef2ebd63ee11e1
lmorais <leozonnn@gmail.com>
2021-07-11 17:15:05 -0300

    feat: configure tools routes for new methods

--- src/modules/tools/infra/http/routes/toolsRoutes.ts
+++ src/modules/tools/infra/http/routes/toolsRoutes.ts
@@ -5,6 +5,8 @@ import ToolsController from '../controller/ToolsController';
 const toolsRouter = Router();
 const toolsController = new ToolsController();
 
+toolsRouter.get('/', toolsController.list);
 toolsRouter.post('/', toolsController.create);
+toolsRouter.post('/', toolsController.delete);
 
 export default toolsRouter;
