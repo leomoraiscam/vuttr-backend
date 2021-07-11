4202ef0591bd4a4a0517b4c5ca9e28420ff38ef7
lmorais <leozonnn@gmail.com>
2021-07-11 17:14:27 -0300

    feat: implementation new methods in controller

--- src/modules/tools/infra/http/controller/ToolsController.ts
+++ src/modules/tools/infra/http/controller/ToolsController.ts
@@ -2,8 +2,22 @@ import { Response, Request } from 'express';
 import { container } from 'tsyringe';
 
 import CreateToolService from '../../../services/CreateToolService';
+import DeleteToolService from '../../../services/DeleteToolsService.';
+import ListToolService from '../../../services/ListToolService';
 
 class ToolsController {
+  public async list(request: Request, response: Response): Promise<Response> {
+    const { tag } = request.query;
+
+    const toolsRepository = container.resolve(ListToolService);
+
+    const tagFilter = tag ? tag.toString() : undefined;
+
+    const filterTools = await toolsRepository.execute(tagFilter);
+
+    return response.json(filterTools);
+  }
+
   public async create(request: Request, response: Response): Promise<Response> {
     const { title, link, description, tags } = request.body;
 
@@ -17,6 +31,16 @@ class ToolsController {
     });
 
     return response.json(tool);
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const deleteTool = container.resolve(DeleteToolService);
+
+    await deleteTool.execute(id);
+
+    return response.status(204).send();
   }
 }
 
